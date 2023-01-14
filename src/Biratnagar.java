c592613fd269c903994315898f23236d103ed6ce
unknown <henendra.giri15@gmail.com>
2023-01-14 12:56:31 +0545

    first commit

---
+++ src/Biratnagar.java
@@ -0,0 +1,7 @@
+public class Biratnagar {
+    public static void main(String[] args) {
+        String message = "hello world" + "!!!!";
+        System.out.println(message);
+
+    }
+}
---
+++ src/helloworld.java
@@ -0,0 +1,14 @@
+import javax.xml.namespace.QName;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+
+public class helloworld {
+    public static void main(String[] args) {
+        for(int i = 5 ; i <50; i++)
+            System.out.println("Hello World");
+
+
+
+    }
+}
